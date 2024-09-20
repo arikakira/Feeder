a3d04608b48c1f48c39c6329cb6f0815f3b5e570
arikakira <samanthal242@nycstudents.net>
2024-09-19 20:43:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,22 +2,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        /*
         Feeder g = new Feeder(500);
         g.simulateOneDay(12);
         System.out.println("First Feeder: " + g.getCurrentFood());
-        
-        Feeder h = new Feeder(1000);
-        h.simulateOneDay(22);
-        System.out.println("Second Feeder: " + h.getCurrentFood());
 
         Feeder i = new Feeder(100);
         i.simulateOneDay(5);
-        System.out.println(i);
-        */
+        System.out.println("Second Feeder " + i);
 
         Feeder a = new Feeder(1500);
         System.out.println("Days: " + a.simulateManyDays(11, 5));
-        System.out.println(a);
+        System.out.println("Third Feeder " + a);
     }
 }
